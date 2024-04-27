bf18f3c1b8a5379d8f7bc5c626631f27d5c4ebe8
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-04-27 18:14:31 -0300

    text-center

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -41,7 +41,7 @@ export function Login() {
 
     return (
         <div className="flex w-full h-screen items-center justify-center flex-col">
-            <Link to={"/"}>
+            <Link to={"/"} className="text-center">
                 <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Controle de
                     <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent"> Costuras</span></h1>
             </Link>
